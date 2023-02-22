efb4a2cea1fc62e88085bc0be133b3f062b32571
Mat_Fisher <matvey@rybakov.pro>
2023-02-22 18:01:51 +0300

    Проверка числа на четность

---
+++ 3_mouth_work/T6_EvenNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿double numA;
+Console.WriteLine("Введите число");
+numA = double.Parse(Console.ReadLine());
+
+if(numA % 2 ==0)
+{
+    Console.WriteLine("Число четное");
+}
+else 
+{
+    Console.WriteLine("Число нечетное");
+}
