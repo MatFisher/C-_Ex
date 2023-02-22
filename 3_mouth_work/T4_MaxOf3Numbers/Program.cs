23d8f4cdf6f03a337e88cde2516c031f3de1290e
Mat_Fisher <matvey@rybakov.pro>
2023-02-22 17:31:57 +0300

    Максимальное из трех чисел

---
+++ 3_mouth_work/T4_MaxOf3Numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿double numA, numB, numC;
+Console.WriteLine("Введите первое число");
+numA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numB = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+numC = double.Parse(Console.ReadLine());
+double max=numA;
+
+if(max < numB) max = numB;
+if(max < numC) max = numC;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
