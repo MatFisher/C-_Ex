7b6f296f6c687813d2f4654a40d967cf9eca03ac
Mat_Fisher <matvey@rybakov.pro>
2023-02-22 16:35:26 +0300

    Показываем большее и меньшее число

---
+++ 3_mouth_work/T2_ViewBigSmallNum/Program.cs
@@ -0,0 +1,19 @@
+﻿double numA, numB;
+Console.WriteLine("Введите первое число");
+numA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+numB = double.Parse(Console.ReadLine());
+if(numA>numB)
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(numA);
+    Console.Write("Меньшее число = ");
+    Console.Write(numB);
+}
+else
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(numB);
+    Console.Write("Меньшее число = ");
+    Console.Write(numA);
+}
