9746757d59bdb18b3d3758c0c024702132cd0438
Mat_Fisher <matvey@rybakov.pro>
2023-02-22 17:18:31 +0300

    Вывести день недели по номеру

---
+++ 3_mouth_work/T3_DayWeekNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿int number;
+Console.WriteLine("Введите номер дня недели");
+number = int.Parse(Console.ReadLine());
+
+if(number==1) Console.Write("Понедельник");
+if(number==2) Console.Write("Вторник");
+if(number==3) Console.Write("Среда");
+if(number==4) Console.Write("Четверг");
+if(number==5) Console.Write("Пятница");
+if(number==6) Console.Write("Суббота");
+if(number==7) Console.Write("Воскресенье");
+if(number > 7) Console.Write("Неверный номер");
