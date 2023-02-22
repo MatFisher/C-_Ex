970eaa75b9333670cb47b84a20c53f3a11964153
Mat_Fisher <matvey@rybakov.pro>
2023-02-22 16:03:02 +0300

    Вывести квадрат введенного числа

---
+++ 3_mouth_work/T0_NumSquare/Program.cs
@@ -0,0 +1,6 @@
+﻿int number;
+Console.WriteLine("Введите целое число");
+number = int.Parse(Console.ReadLine());
+number = number*number;
+Console.Write("Квадрат числа равен ");
+Console.WriteLine(number);
