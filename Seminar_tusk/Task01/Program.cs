42202e04565eb88373ebcf5ee8d9afa19b7558a5
Mat_Fisher <matvey@rybakov.pro>
2023-02-23 15:03:38 +0300

    Задачи семинара

---
+++ Seminar_tusk/Task01/Program.cs
@@ -0,0 +1,14 @@
+﻿double numA, numB;
+Console.WriteLine("Введите большее целое число");
+numA = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число для проверки");
+numB = double.Parse(Console.ReadLine());
+double index = 1;
+if(index==numA/(numB*numB))
+{
+    Console.WriteLine("Квадратом является");
+}
+else
+{
+    Console.WriteLine("Квадратом не является");
+}
---
+++ Seminar_tusk/Task03/Program.cs
@@ -0,0 +1,12 @@
+﻿int number;
+Console.WriteLine("Введите номер дня недели");
+number = int.Parse(Console.ReadLine());
+
+if(number==1) Console.Write("Понедельник");
+else if(number==2) Console.Write("Вторник");
+else if(number==3) Console.Write("Среда");
+else if(number==4) Console.Write("Четверг")
+else if(number==5) Console.Write("Пятница");
+else if(number==6) Console.Write("Суббота");
+else if(number==7) Console.Write("Воскресенье");
+else Console.Write("Неверный номер");
