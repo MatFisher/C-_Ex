5e9f382482a36a06dd008b9112168adb742ecb1c
Mat_Fisher <matvey@rybakov.pro>
2023-02-23 15:14:31 +0300

    Задача по выводу от -n до n

---
+++ Seminar_tusk/Task05/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number > 0)
+{
+    int count = -number;
+    while(count<=number)
+    {
+        Console.Write (count + " ");
+        count = count+1;
+    
+    }
+
+}
+else 
+{
+    Console.WriteLine ("Введено не натуральное число");
+}
