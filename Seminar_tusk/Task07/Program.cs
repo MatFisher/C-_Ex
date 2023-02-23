50dbb7b2f809d09c8d8a1ba64cdef9ae6b3c0d8d
Mat_Fisher <matvey@rybakov.pro>
2023-02-23 15:22:31 +0300

    Последняя цифра числа

---
+++ Seminar_tusk/Task07/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int last = number % 10;
+Console.Write("Последняя цифра = ");
+Console.WriteLine(last);
