697c4da7bc41ebc69b15f8822981a97bbe8e7ab8
Mat_Fisher <matvey@rybakov.pro>
2023-02-23 15:32:26 +0300

    Дописал решение в седьмой задаче

--- Seminar_tusk/Task07/Program.cs
+++ Seminar_tusk/Task07/Program.cs
@@ -1,5 +1,10 @@
 ﻿Console.WriteLine("Введите трехзначное число");
 int number = Convert.ToInt32(Console.ReadLine());
-int last = number % 10;
-Console.Write("Последняя цифра = ");
-Console.WriteLine(last);
+if (number < 0)
+number = -number;
+if (number >= 100 && number <= 999)
+{
+    int last = number % 10;
+    Console.Write("Последняя цифра = ");
+    Console.WriteLine(last);
+}
