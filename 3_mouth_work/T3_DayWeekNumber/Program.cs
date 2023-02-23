b6c81c311ae26af62c8eabf319f3aba1a6b1a434
Mat_Fisher <matvey@rybakov.pro>
2023-02-23 15:03:02 +0300

    Исправил задачу 3

--- 3_mouth_work/T3_DayWeekNumber/Program.cs
+++ 3_mouth_work/T3_DayWeekNumber/Program.cs
@@ -3,10 +3,10 @@ Console.WriteLine("Введите номер дня недели");
 number = int.Parse(Console.ReadLine());
 
 if(number==1) Console.Write("Понедельник");
-if(number==2) Console.Write("Вторник");
-if(number==3) Console.Write("Среда");
-if(number==4) Console.Write("Четверг");
-if(number==5) Console.Write("Пятница");
-if(number==6) Console.Write("Суббота");
-if(number==7) Console.Write("Воскресенье");
-if(number > 7) Console.Write("Неверный номер");
+else if(number==2) Console.Write("Вторник");
+else if(number==3) Console.Write("Среда");
+else if(number==4) Console.Write("Четверг");
+else if(number==5) Console.Write("Пятница");
+else if(number==6) Console.Write("Суббота");
+else if(number==7) Console.Write("Воскресенье");
+else Console.Write("Неверный номер");
