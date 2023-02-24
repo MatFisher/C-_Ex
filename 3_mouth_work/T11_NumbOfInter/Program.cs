a5ddd746dc98012734c80fb0bb572b57ef64b42b
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 12:20:49 +0300

    Дано число из отрезка [10, 99]. Показать наибольшую цифру числа

---
+++ 3_mouth_work/T11_NumbOfInter/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 10 && number <=99)
+{ 
+    int last = (number % 100)/10;
+    int first = (number % 10);
+    if (first>last){
+    Console.Write("Наибольшая цифра числа = ");
+    Console.WriteLine(first);
+}
+else {Console.Write("Наибольшая цифра числа = ");
+    Console.WriteLine(last);
+}
+}
+else Console.WriteLine("Заданное число не входит в отрезок");
