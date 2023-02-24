a49f763404120586d09f651dc22e81777ffad15d
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 12:54:54 +0300

    Выяснить, кратно ли число заданному, если нет, вывести остаток

---
+++ 3_mouth_work/T13_NumbMultRemains/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите большее число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число для проверки");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int last = numA % numB;
+if(last==0)
+{
+    Console.Write("Число делится без остатка");
+}
+else 
+{
+    Console.Write("Остаток от деления равен = ");
+    Console.WriteLine(last);
+}
