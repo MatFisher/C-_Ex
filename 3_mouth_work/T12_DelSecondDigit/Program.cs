a76b812b6edbe54155186dca0b33573dd9bee697
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 12:41:46 +0300

    Удалить вторую цифру трёхзначного числа

---
+++ 3_mouth_work/T12_DelSecondDigit/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+number = -number;
+if (number >= 100 && number <= 999)
+{
+    int last = (number % 100)/10;
+    last = number - last*10;
+    Console.Write("Число без второй цифры = ");
+    Console.WriteLine(last);
+}
