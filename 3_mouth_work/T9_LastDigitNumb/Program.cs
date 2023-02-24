510e61d0d2c84b21c2f427c06c3e9d6c5fb26b4e
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 11:46:15 +0300

    Показать вторую цифру трёхзначного числа

---
+++ 3_mouth_work/T9_LastDigitNumb/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0)
+number = -number;
+if (number >= 100 && number <= 999)
+{
+    int last = number % 10;
+    Console.Write("Последняя цифра = ");
+    Console.WriteLine(last);
+}
