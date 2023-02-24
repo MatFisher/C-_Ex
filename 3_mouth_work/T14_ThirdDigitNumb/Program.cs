4a343779297f9112e97dc57aadad1e6f28c473ff
Mat_Fisher <matvey@rybakov.pro>
2023-02-24 13:03:21 +0300

    Найти третью цифру числа или сообщить, что её нет

---
+++ 3_mouth_work/T14_ThirdDigitNumb/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int last = (number % 1000)/100;
+if (last==0) Console.Write("Цифра отсутствует");
+else 
+{
+    Console.Write("Третья цифра числа = ");
+    Console.WriteLine(last);
+}
