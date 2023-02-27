51ad05f3c1082fa9f045e7d4af4b48d6b8bef78d
Mat_Fisher <matvey@rybakov.pro>
2023-02-27 11:55:21 +0300

    Прошла вторая лекция

---
+++ Lessons_tusk/Lesson 2/T2_Array/Program.cs
@@ -0,0 +1,32 @@
+﻿//int[] array = { 11, 21, 31, 41, 15, 61, 17, 18, 19 };
+//array[0] = 12;
+//Console.WriteLine(array[0]);
+
+//int max =
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
