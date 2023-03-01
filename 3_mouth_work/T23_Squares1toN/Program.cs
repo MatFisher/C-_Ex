26c3d25888191272a7497fe33d5986a3c79a040c
Mat_Fisher <matvey@rybakov.pro>
2023-03-01 12:15:18 +0300

    Таблица квадратов от 1 до N

---
+++ 3_mouth_work/T23_Squares1toN/Program.cs
@@ -0,0 +1,43 @@
+﻿
+void NaturArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index]=index+1;
+        index++;
+    }
+}
+
+// void PrintArray(int[] arr)
+// {
+//     int count = arr.Length;
+//     int position = 0;
+//     while(position < count)
+//     {
+//         Console.WriteLine(arr[position]);
+//         position++;
+//     }
+// }
+
+void SquareArray(int[] ar)
+{
+    int longer = ar.Length;
+    int pos = 0;
+    while(pos < longer)
+    {
+        Console.Write($"{ar[pos]} = ");
+        ar[pos] = ar[pos] * ar[pos];
+        Console.WriteLine(ar[pos]);
+        pos++;
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] m = new int[number];
+NaturArray(m);
+Console.WriteLine();
+SquareArray(m);
