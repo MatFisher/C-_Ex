03dbaf5894d2266d7f5548a0aab27e2b4baf474a
Mat_Fisher <matvey@rybakov.pro>
2023-03-01 15:55:53 +0300

    Показать кубы, оканчивающиеся на четную цифру

---
+++ 3_mouth_work/T30_CubeMod2/Program.cs
@@ -0,0 +1,46 @@
+﻿
+void NaturArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index]=index+1;
+        index++;
+    }
+}
+
+// void PrintArray(int[] arr)
+// {
+//     int count = arr.Length;
+//     int position = 0;
+//     while(position < count)
+//     {
+//         Console.WriteLine(arr[position]);
+//         position++;
+//     }
+// }
+
+void Square3Array(int[] ar)
+{
+    int longer = ar.Length;
+    int pos = 0;
+    while(pos < longer)
+    {
+        if(ar[pos] % 2 == 0)
+        {
+            Console.Write($"{ar[pos]} = ");
+            ar[pos] = ar[pos] * ar[pos] * ar[pos];
+            Console.WriteLine(ar[pos]);
+        }
+        pos++;
+    }
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] m = new int[number];
+NaturArray(m);
+Console.WriteLine();
+Square3Array(m);
