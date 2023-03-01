e351b0c06826d3c1c77ac8b91bdb3976e83231e5
Mat_Fisher <matvey@rybakov.pro>
2023-03-01 17:06:19 +0300

    Замена элементов массива на противоположные

---
+++ 3_mouth_work/T34_InsertArray/Program.cs
@@ -0,0 +1,43 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(-9, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Write($" {col[position]} ");
+        position++;
+    }
+}
+
+void InsertElements(int[] array)
+{
+    int size = array.Length;
+    int ind = 0;
+    while(ind < size)
+    {
+        array[ind] = -array[ind];
+        ind++;
+    }
+}
+
+int[] m = new int[12];
+
+FillArray(m);
+Console.WriteLine();
+PrintArray(m);
+Console.WriteLine();
+InsertElements(m);
+Console.WriteLine();
+PrintArray(m);
+Console.WriteLine();
