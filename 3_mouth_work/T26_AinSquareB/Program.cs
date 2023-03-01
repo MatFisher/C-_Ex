73481682e770a455c62af001b8dbe60a919e5178
Mat_Fisher <matvey@rybakov.pro>
2023-03-01 15:04:13 +0300

    Возведите А в степень В используя цикл

---
+++ 3_mouth_work/T26_AinSquareB/Program.cs
@@ -0,0 +1,19 @@
+﻿int Exponent(int numA, int numB)
+{
+    int index = 1;
+    int numHelp = numA;
+    while (index < numB)
+    {
+        numA = numA * numHelp;
+        index++;
+    }
+    return numA;
+}
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int exp = Exponent(numberA, numberB);
+Console.WriteLine($"Результат = {exp}");
