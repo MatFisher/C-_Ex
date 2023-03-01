e83ba05e55255a6c2e6c223cf8b21745795d8b3d
Mat_Fisher <matvey@rybakov.pro>
2023-03-01 15:31:32 +0300

    Вычислить произведение чисел от 1 до N

---
+++ 3_mouth_work/T29_Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿void NaturArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while(index < length)
+    {
+        array[index]=index+1;
+        index++;
+    }
+}
+
+void Factorial(int[] ar)
+{
+    int longer = ar.Length;
+    int pos = 0;
+    int fact = 1;
+    while(pos < longer)
+    {
+        fact = fact * ar[pos];
+        pos++;
+    }
+    Console.WriteLine($"Факториал числа = {fact}");
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] m = new int[number];
+NaturArray(m);
+Factorial(m);
