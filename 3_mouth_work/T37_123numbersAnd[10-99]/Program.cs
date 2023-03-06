6b576f2c63ff90498fdf5d5c5961acc9b3afe269
Mat_Fisher <matvey@rybakov.pro>
2023-03-06 17:17:37 +0300

    В одномерном массиве из 123 чисел найти элементы из отрезка [10, 99]

---
+++ 3_mouth_work/T37_123numbersAnd[10-99]/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(1, 130);
+    }
+}
+
+int NumbOfCut(int[] arr)
+{
+    int size = arr.Length;
+    int numb = 0;
+    for(int ind = 0; ind < size; ind++)
+    {
+        if(arr[ind] < 100 && arr[ind] > 9) numb = numb+1;
+    }
+    return numb;
+}
+
+
+int[] array = new int[123];
+
+FillArray(array);
+Console.WriteLine();
+int result = NumbOfCut(array);
+Console.WriteLine($"Количество чисел из отрезка = {result}");
