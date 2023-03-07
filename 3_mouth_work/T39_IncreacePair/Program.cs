14f183d0e9cd257f2112468a02ec21f32ff7b56f
Mat_Fisher <matvey@rybakov.pro>
2023-03-07 11:21:40 +0300

    Найти произведение пар чисел в массиве

---
+++ 3_mouth_work/T39_IncreacePair/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillPrintArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(1, 20);
+        Console.Write($" {collection[index]} ");
+    }
+}
+
+void CompArray(int[] arr)
+{
+    int size = arr.Length;
+    int temp = 0;
+    Console.WriteLine("Произведения крайних элементов равны");
+    for(int ind = 0; ind < size / 2; ind++)
+    {
+        temp = arr[ind] * arr[size - ind - 1];
+        Console.Write($" {temp} ");
+    }
+}
+
+int[] array = new int[10];
+FillPrintArray(array);
+Console.WriteLine();
+CompArray(array);
+Console.WriteLine();
