79a075bc19917fa42e9ba705bb8b351bde836c71
Mat_Fisher <matvey@rybakov.pro>
2023-03-07 12:36:59 +0300

    Проверить, являются ли числа сторонами треугольника

---
+++ 3_mouth_work/T41_Pifagor/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите сторону А");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону B");
+int numB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону C");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+string Pifagor(int a, int b, int c)
+{
+    double temp = Math.Sqrt(a*a+b*b);
+    string res;
+    if(c == temp) res = "true";
+    else res = "false";
+    return res ;
+}   
+
+string result = Pifagor(numA, numB, numC);
+Console.WriteLine(result);
