581ae1704057c34e0db31c553ae29134be92d25d
Mat_Fisher <matvey@rybakov.pro>
2023-03-09 14:43:43 +0300

    Семинар 5 задачи

---
+++ Seminar_tusk/Task33/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("["); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else
+            Console.Write($"{arr[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNumber(int[] ar, int num)
+{
+    for (int i = 0; i < ar.Length; i++)
+    {
+        if(ar[i] == num) return true;
+    }
+    return false;
+}
+
+int[] array = CreateArrayRndInt(10, 0, 10);
+PrintArray(array);
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string output = FindNumber(array, number) ? "Yes" : "No";
+Console.WriteLine(output);
---
+++ Seminar_tusk/Task35/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    for(int index = 0; index < length; index++)
+    {
+        collection[index] = new Random().Next(1, 130);
+    }
+}
+
+int NumbOfCut(int[] arr)
+{
+    int size = arr.Length;
+    int numb = 0;
+    for(int ind = 0; ind < size; ind++)
+    {
+        if(arr[ind] < 100 && arr[ind] > 9) numb = numb+1;
+    }
+    return numb;
+}
+
+
+int[] array = new int[123];
+
+FillArray(array);
+Console.WriteLine();
+int result = NumbOfCut(array);
+Console.WriteLine($"Количество чисел из отрезка = {result}");
+
+
+
+
+
+
+
+
+// int[] CreateArrayRndInt(int size, int min, int max)
+// {
+//     int[] arr = new int[size];
+//     Random rnd = new Random();
+//     for (int i = 0; i < size; i++)
+//     {
+//         arr[i] = rnd.Next(min, max + 1);
+//     }
+//     return arr;
+// }
