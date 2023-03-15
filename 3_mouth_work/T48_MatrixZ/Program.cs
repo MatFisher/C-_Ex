16a1a1087a0c4b06885d7effca11e911dc7e7c16
Mat_Fisher <matvey@rybakov.pro>
2023-03-15 11:52:16 +0300

    Задачи 48-51

---
+++ 3_mouth_work/T48_MatrixZ/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] FillMatrixRndInt(int[,] matrix)
+{
+     for (int i = 0; i < matrix.GetLength(0); i++)
+     {
+        for(int j = 0; j < matrix.GetLength(1); j++) 
+        {
+            matrix[i, j] = new Random().Next(1,10); //
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+int[,] matr = new int [3, 4];
+Console.WriteLine();
+matr = FillMatrixRndInt(matr);
+PrintMatrix(matr);
+Console.WriteLine();
---
+++ 3_mouth_work/T49_MatrixR/Program.cs
@@ -0,0 +1,31 @@
+﻿double[,] matrix = new double[4, 5];
+Random rnd = new Random();
+matrix = CreateMatrixRndDouble(matrix);
+Console.WriteLine();
+PrintMatrix(matrix);
+Console.WriteLine();
+
+double[,] CreateMatrixRndDouble(double[,] matr)
+{ 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    { 
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        { 
+            matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);
+            matr[i,j] = Math.Round(matr[i,j], 1);
+        }
+    }
+    return matr;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
